4021fd51b75786c285ad2373863d0fd650b339b3
Liony94 <gui.prouchandy@gmail.com>
2023-09-19 01:04:41 +0200

    Api authentication terminé

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,33 +2,33 @@
 
 namespace App\Controller;
 
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthController extends AbstractController
 {
-    #[Route(path: '/api/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): JsonResponse
+    #[Route(path: '/api/login', name: 'app_login', methods: ['POST'])]
+    public function login(UserInterface $user, JWTTokenManagerInterface $JWTManager): JsonResponse
     {
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
+        return new JsonResponse(['token' => $JWTManager->create($user)]);
+    }
 
-        if ($error) {
-            return new JsonResponse([
-                'success' => false,
-                'message' => 'Authentication failed.',
-                'error' => $error->getMessage(),
-                'last_username' => $lastUsername
-            ], 401);
+    #[Route(path: '/api/verify_token', name: 'api_verify_token', methods: ['GET'])]
+    public function verifyToken(TokenStorageInterface $tokenStorage): JsonResponse
+    {
+        $token = $tokenStorage->getToken();
+        if ($token && $token->getUser() instanceof UserInterface) {
+            // Le token est valide
+            return new JsonResponse(['valid' => true], Response::HTTP_OK);
         }
-
-        return new JsonResponse([
-            'success' => true,
-            'message' => 'Authentication succeeded.'
-        ]);
+        // Le token n'est pas valide
+        return new JsonResponse(['valid' => false], Response::HTTP_UNAUTHORIZED);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
